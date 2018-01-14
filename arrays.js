56b656ae01b13a86da3dec0db941cbb59c4a2972
Andrew Trent <andrewtrentdesign@gmail.com>
2018-01-14 17:34:08 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+function addElementToBeginningOfArray(a,e){
+b = [e, ...a]
+return b
+}
+function destructivelyAddElementToBeginningOfArray(a,e){
+  a = [e, ...a]
+  return a;
+}
+function addElementToEndOfArray(a,e){
+b = [...a, e]
+return b
+}
+function destructivelyAddElementToEndOfArray(a,e){
+  c = a.push(e);
+  return c;
+}
